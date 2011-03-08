14da0870dfb27f81212505890bb3c13a1b2a9642
Stefano Verna <stefano.verna@gmail.com>
2011-03-08 15:14:34 +0100

    Trying to add indexes

--- app/models/err.rb
+++ app/models/err.rb
@@ -1,7 +1,7 @@
 class Err
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :klass
   field :component
   field :action
@@ -9,39 +9,42 @@ class Err
   field :fingerprint
   field :last_notice_at, :type => DateTime
   field :resolved, :type => Boolean, :default => false
-  
+
   referenced_in :app
   embeds_many :notices
-  
+
   validates_presence_of :klass, :environment
-  
+
   scope :resolved, where(:resolved => true)
   scope :unresolved, where(:resolved => false)
   scope :ordered, order_by(:last_notice_at.desc)
   scope :in_env, lambda {|env| where(:environment => env)}
   scope :for_apps, lambda {|apps| where(:app_id.in => apps.all.map(&:id))}
-  
+
+  index :last_notice_at
+  index :resolved
+
   def self.for(attrs)
     app = attrs.delete(:app)
     app.errs.where(attrs).first || app.errs.create!(attrs)
   end
-  
+
   def resolve!
     self.update_attributes!(:resolved => true)
   end
-  
+
   def unresolved?
     !resolved?
   end
-  
+
   def where
     where = component.dup
     where << "##{action}" if action.present?
     where
   end
-  
+
   def message
     notices.first.message || klass
   end
-  
+
 end
